59423d95792ca1c60b228d4793d4ea0be4ce589f
maddie-spec <salimumaddeino14@gmail.com>
2023-03-16 10:55:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
- #include <stdio.h>
-
- int main(void) 
+#include <stdio.h>
+int main(void)
  /**
   *
   * main-Entry point
   *
   * return:Always 0(success)
-  */	
- {  
+  */
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
- } 
+}
