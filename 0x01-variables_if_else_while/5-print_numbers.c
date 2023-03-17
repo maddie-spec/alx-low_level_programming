2373ee2c32597924d7965056a969515eafa3b206
maddie-spec <salimumaddeino14@gmail.com>
2023-03-17 15:03:00 -0700

    5-prints

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	{
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	}
+
+	printf("\n");
+	return (0);
+}
