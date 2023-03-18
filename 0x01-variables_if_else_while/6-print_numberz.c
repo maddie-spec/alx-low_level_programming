6462c3e4a6cec6a224a877edbaa60205d2c8ea62
maddie-spec <salimumaddeino14@gmail.com>
2023-03-18 02:37:47 -0700

    ./6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints 0 to 9
+ * main - to print 0 to 9
  * Return: Always 0
  */
 int main(void)
@@ -9,8 +9,9 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-	
-		putchar(num);
+
+		putchar((num % 10) + '0');
 
 	putchar('\n');
+	return (0);
 }
