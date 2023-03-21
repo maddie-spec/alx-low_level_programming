4279e988e88ef011a0e71c71eed7eb96fa135959
maddie-spec <salimumaddeino14@gmail.com>
2023-03-21 10:37:52 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - to print sign
+ * @n: the variable
+ * Return: 0 , 1 -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
