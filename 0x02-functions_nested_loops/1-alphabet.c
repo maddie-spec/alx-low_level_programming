07a5852ca4b6acf53926f421464a3e452f54eebf
maddie-spec <salimumaddeino14@gmail.com>
2023-03-21 04:57:17 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - to print lower case letters
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+	-putchar('\n');
+
+}
