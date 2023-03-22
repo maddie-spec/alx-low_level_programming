3d96c2d8da87c9df411bf80e1a7059281ece19b8
maddie-spec <salimumaddeino14@gmail.com>
2023-03-22 13:41:17 -0700

    104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - to prit first 98 fibioncci number
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count;
+	long int first = 1, second = 2;
+	long int next = first + second;
+
+	printf("%lu, ", first);
+	printf("%lu, ", second);
+
+	for (count = 3; count <= 98; count++)
+	{
+		if (count == 98)
+		{
+			printf("%lu\n", next);
+		}
+		else
+		{
+			printf("%lu\n", next);
+		}
+		first = second;
+		second = next;
+
+		next = first + second;
+	}
+	return (0);
+}
