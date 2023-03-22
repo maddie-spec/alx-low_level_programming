08df4df42246a38e1d3dec8628948828cc767082
maddie-spec <salimumaddeino14@gmail.com>
2023-03-22 04:07:03 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - to sum up two integers
+ * @a: a variable
+ * @b: a variable
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+
+	int sum = a + b;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,11 @@ int print_last_digit(int d)
 	int ld = d % 10;
 
 	if (ld < 0)
-		ld *= -1;
-
-	_putchar(ld + '0');
-
-	return (0);
+	{
+		_putchar('-');
+		ld = -ld;
+	}
+	
+	_putchar(ld);
 
 }
