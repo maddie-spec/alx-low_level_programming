5edec971e7f05d7fcf06bedd31cc38a6c3a50a97
maddie-spec <salimumaddeino14@gmail.com>
2023-03-23 06:32:31 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - to multiply a and b
+ * @a: a variable
+ * @b: a variable
+ * Return: tiply
+ */
+int mul(int a, int b)
+{
+	int tiply = a * b;
+
+	return (tiply);
+}
