c1fe5c30d5fe20114e7bce11ec73e93746d75ff2
maddie-spec <salimumaddeino14@gmail.com>
2023-03-23 07:36:49 -0700

    print-most -numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,9 @@ void print_most_numbers(void)
 
 	for (n = 48; n < 58; n++)
 	{
-		if (n == 2 || n == 4)
+		if (n == 50 || n == 52)
 			continue;
-		else
-			_putchar(n);
+		_putchar(n);
 	}
 	_putchar('\n');
 }
