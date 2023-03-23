cb09422a9f880f80fe24e02acca9f86f823cb345
maddie-spec <salimumaddeino14@gmail.com>
2023-03-23 11:24:28 -0700

    ./6-lines | cat -e

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - to print lines
+ * Return: 0
+ * @n: a variable
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	
+}
