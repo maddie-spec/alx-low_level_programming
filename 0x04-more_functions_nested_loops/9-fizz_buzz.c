de97d535f330b21b823439e56527121d086c48f2
maddie-spec <salimumaddeino14@gmail.com>
2023-03-24 00:51:51 -0700

    ./9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - to print 1- 100
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 != 0))
+		{
+			printf("%s", f);
+		}
+		else if ((a % 5 == 0) && (a % 3 != 0))
+		{
+			printf("%s", b);
+		}
+		else if ((a % 5 == 0) && (a % 3 == 0))
+		{
+			printf("%s", fb);
+		}
+		else
+		{
+			printf("%d", a);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
