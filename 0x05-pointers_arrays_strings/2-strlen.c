984397239ec9fecd505c7dfd405a64c00bda1b14
maddie-spec <salimumaddeino14@gmail.com>
2023-03-27 10:00:54 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - to find length of a string
+ * Return: 0
+ * @s: a pointer
+ */
+int _strlen(char *s)
+{	
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+
+}
